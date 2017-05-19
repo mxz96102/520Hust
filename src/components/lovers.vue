9af4c8ee8a753fdb568df7b89c5dca5c384f3d39
mxz96102 <mxz96102@qq.com>
2017-05-19 12:37:54 +0800

    change api url

--- src/components/lovers.vue
+++ src/components/lovers.vue
@@ -114,7 +114,7 @@
     wx.onMenuShareTimeline({
       title: '520到了，没什么送我的话，麻烦给我表个白蟹蟹！', // 分享标题
       link: location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-      imgUrl: 'http://520.mingdom.cn/stastic/img/pic.jpg', // 分享图标
+      imgUrl: 'http://520.mingdom.cn/static/img/pic.jpg', // 分享图标
       success: function () {
         // 用户确认分享后执行的回调函数
       },
@@ -126,7 +126,7 @@
       title: '520到了，没什么送我的话，麻烦给我表个白蟹蟹！', // 分享标题
       desc: '520 HUST 匿名表白', // 分享描述
       link: location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-      imgUrl: 'http://520.mingdom.cn/stastic/img/pic.jpg', // 分享图标
+      imgUrl: 'http://520.mingdom.cn/static/img/pic.jpg', // 分享图标
       type: '', // 分享类型,music、video或link，不填默认为link
       dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
       success: function () {
@@ -140,7 +140,7 @@
       title: '520到了，没什么送我的话，麻烦给我表个白蟹蟹！', // 分享标题
       desc: '520 HUST 匿名表白', // 分享描述
       link: location.href, // 分享链接
-      imgUrl: 'http://520.mingdom.cn/stastic/img/pic.jpg', // 分享图标
+      imgUrl: 'http://520.mingdom.cn/static/img/pic.jpg', // 分享图标
       success: function () {
         // 用户确认分享后执行的回调函数
       },
@@ -152,7 +152,7 @@
       title: '520到了，没什么送我的话，麻烦给我表个白蟹蟹！', // 分享标题
       desc: '520 HUST 匿名表白', // 分享描述
       link: location.href, // 分享链接
-      imgUrl: 'http://520.mingdom.cn/stastic/img/pic.jpg', // 分享图标
+      imgUrl: 'http://520.mingdom.cn/static/img/pic.jpg', // 分享图标
       success: function () {
         // 用户确认分享后执行的回调函数
       },
